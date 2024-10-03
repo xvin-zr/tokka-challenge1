b0f2914da922e42afbf7777b265f10800dfa45d6
xvin <xvin@bupt.edu.cn>
2024-10-04 00:00:55 +0800

    update fetch realtime transactions with correct API

--- express/src/api/fetch-realtime-txn.ts
+++ express/src/api/fetch-realtime-txn.ts
@@ -1,21 +1,22 @@
 import { ETHERSCAN_API_KEY, USDC_ETH_POOL_ADDRESS } from '.';
+import { lastBlock, txns } from '../data/txns.json';
 
-let lastBlock = 0;
 export default async function fetchRealtimeTxns() {
-    const out: Txn[] = [];
     try {
         const latestBlockResp = await fetch(
             `https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=${ETHERSCAN_API_KEY}`
         ).then((res) => res.json());
         const latestBlock = parseInt(latestBlockResp.result, 16);
 
-        const transactionResp =
-            await fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${USDC_ETH_POOL_ADDRESS}&startblock=${lastBlock}&endblock=${latestBlock}&page=1&offset=50&sort=desc&apikey=${ETHERSCAN_API_KEY}
-`).then((res) => res.json());
+        const transactionResp = await fetch(
+            `https://api.etherscan.io/api?module=account&action=tokentx&address=${USDC_ETH_POOL_ADDRESS}&page=1&offset=100&startblock=${
+                lastBlock ? lastBlock : latestBlock - 99
+            }&endblock=${latestBlock}&sort=desc&apikey=${ETHERSCAN_API_KEY}`
+        ).then((res) => res.json());
         const transactions: Txn[] = transactionResp.result;
         console.log(transactions[0]);
-        out.push(...transactions);
-        return [latestBlock, out] as const;
+        txns.push(...transactions);
+        return [latestBlock, txns] as const;
     } catch (err) {
         throw new Error(`Error fetching transaction: ${err}`);
     }
